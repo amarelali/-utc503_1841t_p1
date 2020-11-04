a63103f5a08b727aeb46606cd2b3ea93002c13db
amarelali <amar.elali@isaea.edu.lb>
2020-11-04 07:08:47 +0200

    commit

--- Devoir noté pour les vacances/solution.py
+++ Devoir noté pour les vacances/solution.py
@@ -3,49 +3,77 @@
 def rendreMonnaieRec(list , monnaie):
     listRendu = []
     if list == [] :
-        print("pas de monnaie a rendu")
+        return listRendu
     else:            
             n = monnaie / list[0]
             listRendu.append(int(n))
+            print(listRendu)
             monnaie = (monnaie - list[0])
-            return listRendu + rendreMonnaie(list[1:] , monnaie)
+            return listRendu + rendreMonnaieRec(list[1:] , monnaie)
         
             
 #2.2
 # def rendreMonnaieListComp(list , monnaie):
 #     listRendu = []
-#     obj = [if list == [] print("pas de monnaie a rendu") else for x in list n = monnaie / x listRendu.append(int(n))  monnaie = (monnaie - x) return  listRendu ]
+#     obj = [if list == [] print("pas de monnaie a rendu") else for x in 
+
+#list n = monnaie / x listRendu.append(int(n))  monnaie = (monnaie - x) 
+
+#return  listRendu ]
 #     print(obj)
 #2.3
 def rendreMonnaie(list , monnaie):
     listRendu = []
     if list == [] :
-        print("pas de monnaie a rendu")
+        listRendu=[0]
+        return listRendu
     else:
         for x in list :
+            if x == 0:
+                listRendu =[0]
+                return listRendu
+            else:
+                n = monnaie / x
             
-            n = monnaie / x
-            listRendu.append(int(n))
-            monnaie = (monnaie - x)
-        return  listRendu
+                listRendu.append(int(n))
+                monnaie = (monnaie - x)
+            return  listRendu
             
-print(rendreMonnaie([5000 , 2000] , 7000))
+print(rendreMonnaie([] , 7000))
 print(rendreMonnaieRec([5000 , 2000] , 7000))
 
 print(rendreMonnaie([5000 , 3000] , 7000))
 print(rendreMonnaieRec([5000 , 3000] , 7000))
 
-#3
+#3.1 recursive
 
-# def toutRendreMonnaie(list , monnaie):
+def toutRendreMonnaie(list , monnaie):
+    toutRendreMonnaie =[]
+    if list == [] :
+        toutRendreMonnaie.append(0) 
+    else:
+      toutRendreMonnaie.append(rendreMonnaieRec(list , monnaie))
+      return toutRendreMonnaie + [[0] + rendreMonnaieRec(list[1:] , monnaie)]
+print(toutRendreMonnaie([5000 , 1000] , 7000))
+#3.3
+# def toutRendreMonnaieItera(list , monnaie):
 #     toutRendreMonnaie =[]
-#     l=[]
-#     toutRendreMonnaie.append(rendreMonnaie(list , monnaie))
-#     for x in toutRendreMonnaie:
-#          for i in range(0,len(x)):
-#             print(toutRendreMonnaie.append(rendreMonnaie(l[i] , monnaie)))
-#     return toutRendreMonnaie
-# print(toutRendreMonnaie([5000 , 1000] , 7000))
+#     l =[]
+#     if list == [] :
+#         toutRendreMonnaie=[0]
+#         return toutRendreMonnaie
+#     else:
+                        
+#           for x in list : 
+#                 if list.index(x)>0:
+#                     l =[0]
+#                     l= l + list[list.index(x):]
+#                     toutRendreMonnaie.append(rendreMonnaie( l , monnaie))
 
+#                 else:
+#                     toutRendreMonnaie.append(rendreMonnaie( list[list.index(x):] , monnaie))
 
-
+#           return toutRendreMonnaie
+        
+# print(toutRendreMonnaieItera([5000 , 1000] , 7000))
+     
